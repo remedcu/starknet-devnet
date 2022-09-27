6e3107f54f1269324e646f14764a139ee8aed367
mikiw <mikiw.pp@gmail.com>
2022-09-27 16:40:47 +0200

    Update misc.py
    [skip ci]

--- starknet_devnet/blueprints/rpc/misc.py
+++ starknet_devnet/blueprints/rpc/misc.py
@@ -3,12 +3,19 @@ RPC miscellaneous endpoints
 """
 
 from __future__ import annotations
+from queue import Empty
 
-from typing import Union
+from typing import Union, List
 
 from starknet_devnet.blueprints.rpc.structures.types import BlockId, BlockNumber, Felt, Address
 from starknet_devnet.state import state
+import collections
 
+def is_empty(a):
+    return not a and isinstance(a, collections.Iterable)
+
+def is_not_empty(a):
+    return not is_empty(a)
 
 async def chain_id() -> str:
     """
@@ -28,30 +35,46 @@ async def syncing() -> Union[dict, bool]:
 
 
 # pylint: disable=redefined-builtin
-async def get_events(from_block: BlockId, to_block: BlockId, address: Address) -> str:
+async def get_events(from_block: BlockId, to_block: BlockId, address: Address, keys: List[Address]) -> str:
     """
     Returns all events matching the given filter
     """
     # TODO: move to the new file?
-    # TODO: what about RESULT_PAGE_REQUEST?
+    # TODO: what about RESULT_PAGE_REQUEST and paging?
+    # TODO: add tests (test case with address 0x not 0x0)
+
     devnet_state = state.starknet_wrapper.get_state()
     print("devnet_state", devnet_state.events)
- 
+
     number_of_blocks_start = from_block
     if to_block == "latest":
         number_of_blocks_end = state.starknet_wrapper.blocks.get_number_of_blocks()
     else:
         number_of_blocks_end = to_block
-    
+
+    # TODO: Refactor this to avoid for if for if and is_not_empty/is_empty/else case with keys
+    events = []
     for i in range(int(number_of_blocks_start), int(number_of_blocks_end)):
         block = state.starknet_wrapper.blocks.get_by_number(i)
         if block.transaction_receipts != ():
-            print('Events at ', i, ': ', block.transaction_receipts[0].events) 
+            for event in block.transaction_receipts[0].events:
+                event_to_add = event
 
-    # TODO: filters
-    # keys as array [a,b,c] = a or b or c
+                if is_not_empty(keys) and bool(set(event.keys) & set(keys)):
+                    event_to_add = event
+                elif is_empty(keys):
+                    event_to_add = event
+                else:
+                    event_to_add = "" # Get rid of this clear
 
-    return devnet_state.events
+                if address == "":
+                    events.append(event_to_add)
+                elif address != "" and event.from_address == int(address, 0):
+                    events.append(event_to_add)
+                    print('adress filtering at ', i, ': ', event_to_add) 
+
+    print("events: ", events)
+    return events
 
 
 async def get_nonce(contract_address: Address) -> Felt:
