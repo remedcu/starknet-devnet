d233439ab0dee2d1601182770e342b2e1b50524b
mikiw <mikiw.pp@gmail.com>
2022-10-07 14:15:37 +0200

    Refactor test data
    [skip ci]

---
+++ test/rpc/test_data/get_events.py
@@ -0,0 +1,60 @@
+BLOCK_0_LATEST = {
+    "from_block": "0",
+    "to_block": "latest",
+}
+
+BLOCK_0_3 = {
+    "from_block": "0",
+    "to_block": "3",
+}
+
+BLOCK_3_4 = {
+    "from_block": "3",
+    "to_block": "4",
+}
+
+BLOCK_0_LATEST_CHUNK_3_0 = {
+    "from_block": "0",
+    "to_block": "latest",
+    "chunk_size": 3,
+}
+
+BLOCK_0_LATEST_CHUNK_3_1 = {
+    "from_block": "0",
+    "to_block": "latest",
+    "chunk_size": 3,
+    "continuation_token": "1",
+}
+
+BLOCK_0_LATEST_ADDRESS = {
+    "from_block": "0",
+    "to_block": "latest",
+    "address": "0x62230eA046a9a5fbc261ac77d03c8d41e5d442db2284587570ab46455fd2488",
+}
+
+BLOCK_0_LATEST_KEY = {
+    "from_block": "0",
+    "to_block": "latest",
+    "keys": [
+        "0x99cd8bde557814842a3121e8ddfd433a539b8c9f14bf31ebf108d12e6196e9"
+    ],
+}
+
+BLOCK_0_LATEST_KEYS = {
+    "from_block": "0",
+    "to_block": "latest",
+    "keys": [
+        "0x99cd8bde557814842a3121e8ddfd433a539b8c9f14bf31ebf108d12e6196e9",
+        "0x3db3da4221c078e78bd987e54e1cc24570d89a7002cefa33e548d6c72c73f9d",
+    ],
+}
+
+BLOCK_0_LATEST_ADDRESS_KEYS = {
+    "from_block": "0",
+    "to_block": "latest",
+    "address": "0x62230eA046a9a5fbc261ac77d03c8d41e5d442db2284587570ab46455fd2488",
+    "keys": [
+        "0x99cd8bde557814842a3121e8ddfd433a539b8c9f14bf31ebf108d12e6196e9",
+        "0x3db3da4221c078e78bd987e54e1cc24570d89a7002cefa33e548d6c72c73f9d",
+    ],
+}
--- test/rpc/test_rpc_misc.py
+++ test/rpc/test_rpc_misc.py
@@ -11,6 +11,17 @@ from starkware.starknet.core.os.class_hash import compute_class_hash
 from starknet_devnet.blueprints.rpc.structures.types import BlockHashDict
 from starknet_devnet.blueprints.rpc.utils import rpc_root
 from starknet_devnet.general_config import DEFAULT_GENERAL_CONFIG
+from .test_data.get_events import (
+    BLOCK_0_LATEST,
+    BLOCK_0_3,
+    BLOCK_3_4,
+    BLOCK_0_LATEST_CHUNK_3_0,
+    BLOCK_0_LATEST_CHUNK_3_1,
+    BLOCK_0_LATEST_ADDRESS,
+    BLOCK_0_LATEST_KEY,
+    BLOCK_0_LATEST_KEYS,
+    BLOCK_0_LATEST_ADDRESS_KEYS,
+)
 
 from .rpc_utils import rpc_call, gateway_call, get_block_with_transaction, pad_zero
 
@@ -158,90 +169,47 @@ def test_call_with_invalid_params(params):
     [
         (
             [*PREDEPLOY_ACCOUNT_CLI_ARGS],
-            {
-                "from_block": "0",
-                "to_block": "latest",
-            },
+            BLOCK_0_LATEST,
             4,
         ),
         (
             [*PREDEPLOY_ACCOUNT_CLI_ARGS],
-            {
-                "from_block": "0",
-                "to_block": "3",
-            },
+            BLOCK_0_3,
             2,
         ),
         (
             [*PREDEPLOY_ACCOUNT_CLI_ARGS],
-            {
-                "from_block": "3",
-                "to_block": "4",
-            },
+            BLOCK_3_4,
             2,
         ),
         (
             [*PREDEPLOY_ACCOUNT_CLI_ARGS],
-            {
-                "from_block": "0",
-                "to_block": "latest",
-                "chunk_size": 3,
-            },
+            BLOCK_0_LATEST_CHUNK_3_0,
             3,
         ),
         (
             [*PREDEPLOY_ACCOUNT_CLI_ARGS],
-            {
-                "from_block": "0",
-                "to_block": "latest",
-                "chunk_size": 3,
-                "continuation_token": "1",
-            },
+            BLOCK_0_LATEST_CHUNK_3_1,
             1,
         ),
         (
             [*PREDEPLOY_ACCOUNT_CLI_ARGS],
-            {
-                "from_block": "0",
-                "to_block": "latest",
-                "address": "0x62230eA046a9a5fbc261ac77d03c8d41e5d442db2284587570ab46455fd2488",
-            },
+            BLOCK_0_LATEST_ADDRESS,
             2,
         ),
         (
             [*PREDEPLOY_ACCOUNT_CLI_ARGS],
-            {
-                "from_block": "0",
-                "to_block": "latest",
-                "keys": [
-                    "0x99cd8bde557814842a3121e8ddfd433a539b8c9f14bf31ebf108d12e6196e9"
-                ],
-            },
+            BLOCK_0_LATEST_KEY,
             2,
         ),
         (
             [*PREDEPLOY_ACCOUNT_CLI_ARGS],
-            {
-                "from_block": "0",
-                "to_block": "latest",
-                "keys": [
-                    "0x99cd8bde557814842a3121e8ddfd433a539b8c9f14bf31ebf108d12e6196e9",
-                    "0x3db3da4221c078e78bd987e54e1cc24570d89a7002cefa33e548d6c72c73f9d",
-                ],
-            },
+            BLOCK_0_LATEST_KEYS,
             4,
         ),
         (
             [*PREDEPLOY_ACCOUNT_CLI_ARGS],
-            {
-                "from_block": "0",
-                "to_block": "latest",
-                "address": "0x62230eA046a9a5fbc261ac77d03c8d41e5d442db2284587570ab46455fd2488",
-                "keys": [
-                    "0x99cd8bde557814842a3121e8ddfd433a539b8c9f14bf31ebf108d12e6196e9",
-                    "0x3db3da4221c078e78bd987e54e1cc24570d89a7002cefa33e548d6c72c73f9d",
-                ],
-            },
+            BLOCK_0_LATEST_ADDRESS_KEYS,
             2,
         ),
     ],
